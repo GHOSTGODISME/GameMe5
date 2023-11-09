b33732a8ea8242962178414e16a32d68d8fdeb61
GHOSTGODISME <95931803+GHOSTGODISME@users.noreply.github.com>
2023-11-09 23:42:58 +0800

    ..

---
+++ storage/framework/views/38c4300d59e32faad764628a7d9fcd78.php
@@ -0,0 +1,10 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   <h1>Testing 1234</h1> 
+</body>
+</html><?php /**PATH C:\Users\vickh\OneDrive\Documents\TARUC\Degree\FYP\Source Code\FinalYearProject\laravel\gameme5\resources\views/home.blade.php ENDPATH**/ ?>
